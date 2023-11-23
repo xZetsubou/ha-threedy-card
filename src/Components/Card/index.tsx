1b76a2e102e775805752dc5b037c441b5d8b5d50
Bander <46300268+xZetsubou@users.noreply.github.com>
2023-11-23 12:28:20 +0300

    adjust states letter case

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -62,13 +62,13 @@ const Card = ({ }) => {
     const neumorphicShadow = hass.themes.darkMode ? '-5px -5px 8px rgba(50, 50, 50,.2),5px 5px 8px rgba(0,0,0,.08)' : '-4px -4px 8px rgba(255,255,255,.5),5px 5px 8px rgba(0,0,0,.03)'
     const defaultShadow = 'var( --ha-card-box-shadow, 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12) )'
 
-    const hidden = state !== 'Printing' && !hiddenOverride;
+    const hidden = state.toLowerCase() !== 'printing' && !hiddenOverride;
     const statusColor =
-        state === 'Printing' ?
+        state.toLowerCase() === 'printing' ?
             "#4caf50"
-            : state === "unknown" ?
+            : state.toLowerCase() === "unknown" ?
                 "#f44336"
-                : state === "Operational" ?
+                : state.toLowerCase() === "operational" || state.toLowerCase() === "idle" ?
                     "#00bcd4"
                     : "#ffc107"
 
